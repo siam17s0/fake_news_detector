4b6a06ffdcc5bbb7bc3c299287b1058e7e485a2b
siam17s0 <ahmmedsiam004@gmail.com>
2026-03-20 14:22:38 +0600

    app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
+nltk.download('averaged_perceptron_tagger_eng')
 try:
     nltk.data.find('tokenizers/punkt')
 except LookupError:
